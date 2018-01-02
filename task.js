fb626222b99f852876254eebe65ab608a99d00fe
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-01-02 08:22:00 +0100

    Remove node_modules, modify task.js

--- task.js
+++ task.js
@@ -2,9 +2,10 @@ var fs = require("fs");
 
 //odczytać zawartość katalogu
 fs.readdir("./dir", function(err, files) {
+	if (err) throw err;
 	console.log(files);
 	fs.writeFile("message.txt", files , function(err, data) {//wpisać do nowo utworzonego pliku
-	if (err) throw err;
+		if (err) throw err;
 		console.log("The file has been saved!");
 	});
 });
